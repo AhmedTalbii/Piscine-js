2c504563212ced69c256529a593d7270a84fbed6
ahtalbi <obetox1@gmail.com>
2025-11-26 17:22:18 +0100

    Isaac Newton

--- physics.js
+++ physics.js
@@ -1,5 +1,5 @@
 function getAcceleration(obj) {
-    if (Object.keys().length === 0) return "impossible";
+    if (obj && Object.keys().length === 0) return "impossible";
     let res = obj.f / obj.m;
     res = obj.Δv / obj.Δt;
     res = 2*obj.d / obj.t**2;
