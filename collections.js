4d2dea3623cba4b1a25771266079594358b3c648
ahtalbi <obetox1@gmail.com>
2025-11-26 18:31:31 +0100

    collection

--- collections.js
+++ collections.js
@@ -1,11 +1,18 @@
-const arrToSet = (arr) => new Set(arr);
-const arrToStr = (arr) => arr.join("");
-const setToArr = (set) => [...set];
-const setToStr = (set) => [...set].join("");
-const strToArr = (str) => str.split("");
-const strToSet = (str) => new Set(str.split(""));
-const mapToObj = (map) => Object.fromEntries(map);
-const objToArr = (obj) => [...Object.values(obj)];
-const objToMap = (obj) => new Map(Object.entries(obj));
-const arrToObj = (arr) => new Object.fromEntries(arr.entries());
-const strToObj = (str) => new Object.fromEntries([...str].entries());
+const arrToSet    = (arr) => new Set(arr);
+const arrToStr    = (arr) => arr.join("");
+const setToArr    = (set) => [...set];
+const setToStr    = (set) => [...set].join("");
+const strToArr    = (str) => str.split("");
+const strToSet    = (str) => new Set(str.split(""));
+const mapToObj    = (map) => Object.fromEntries(map);
+const objToArr    = (obj) => [...Object.values(obj)];
+const objToMap    = (obj) => new Map(Object.entries(obj));
+const arrToObj    = (arr) => new Object.fromEntries(arr.entries());
+const strToObj    = (str) => new Object.fromEntries([...str].entries());
+const superTypeOf = (val) => {
+    if (Array.isArray(val)) return 'Array';
+    if (val === null) return 'null';
+    if (val === undefined) return 'undefined';
+    if (Object.prototype.toString.call(val) === "[object Function]") return 'Function';
+    return typeof val;
+}
