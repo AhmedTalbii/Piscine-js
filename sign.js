d23e60966f31d4cedf88386da2c041815154217e
ahtalbi <obetox1@gmail.com>
2025-11-26 16:36:45 +0100

    sign

--- sign.js
+++ sign.js
@@ -1,7 +1,2 @@
-function sign(n) {
-    if (n > 0) return 1;
-    if (n < 0) return -1;
-    return 0;
-}
-
-console.log(sign(-2));
+const sign = (n) => (n > 0) ? 1 : (n < 0) ? -1 : 0;
+const sameSign = (n1, n2) => sign(n1) === sign(n2);
