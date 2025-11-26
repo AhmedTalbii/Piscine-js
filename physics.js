920309881604e7c9ec0dc0a5687b1faea7302a2e
ahtalbi <obetox1@gmail.com>
2025-11-26 17:35:16 +0100

    physics

--- physics.js
+++ physics.js
@@ -1,10 +1,7 @@
 function getAcceleration(obj) {
-    if (obj ?? Object.keys().length === 0) return "impossible";
+    if (!obj ?? Object.keys().length === 0) return "impossible";
     let res = obj.f / obj.m;
-    if (!isFinite(res)) return res;
     res = obj.Δv / obj.Δt;
-    if (!isFinite(res)) return res;
     res = 2*obj.d / obj.t**2;
-    if (!isFinite(res)) return res;
-    return "impossible";
+    return isFinite(res) ? "impossible" : res;
 }
