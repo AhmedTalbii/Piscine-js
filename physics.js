a331a0d4bfa586c95a2cb6c78eefbc638457e73c
ahtalbi <obetox1@gmail.com>
2025-11-26 17:46:53 +0100

    physics

--- physics.js
+++ physics.js
@@ -1,6 +1,6 @@
 function getAcceleration(obj) {
-    if (boj.f, obj.m) return boj.f / obj.m;
-    else if (obj.Δv, obj.Δt) return obj.Δv / obj.Δt;
-    else if (obj.d, obj.t) return 2*obj.d / obj.t**2;
+    if (obj.f && obj.m) return obj.f / obj.m;
+    else if (obj.Δv && obj.Δt) return obj.Δv / obj.Δt;
+    else if (obj.d && obj.t) return 2*obj.d / obj.t**2;
     else return "impossible";
 }
