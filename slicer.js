4c25b3e631c8e91fe4b7a7b1643ec670dc253c7b
ahtalbi <obetox1@gmail.com>
2025-11-28 15:16:11 +0100

    slicer

---
+++ slicer.js
@@ -0,0 +1,19 @@
+// Create a function named slice that works like Array.slice() and String.slice().
+// It takes 3 arguments:
+// string or array to process.
+// starting index.
+// optional ending index.
+
+
+function slice(ele, s, e = ele.length) {
+    const n = ele.length;
+    s = (s < 0) ? (n + s < 0) ? 0 : n + s : s;
+    e = (e < 0) ? n + e : (e > n) ? n : e;
+    let isArr = Array.isArray(ele); res = isArr ? [] : "";
+    while (s < e) {
+        if (isArr) res.push(ele[s]);
+        else res += ele[s];
+        s++;
+    }
+    return res;
+}
