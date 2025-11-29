a1201ee0433a3cd12cf62688deb720dabb0927fa
AhmedTalbii <ahmedtalbi459@gmail.com>
2025-11-29 17:21:08 +0100

    flat js

--- flat.js
+++ flat.js
@@ -1,5 +1,10 @@
-function flat(arr, res) {
-    for (let i in arr) {
-
+function flat(arr, n = 1, res = []) {
+    for (let v of arr) {
+        if (n > 0 && Array.isArray(v)) flat(v, n-1, res);
+        else res.push(v);
     }
+    return res;
 }
+
+console.log([1, 2, [5, 7, [15, 87], 9], 7].flat(2));
+console.log(flat([1, 2, [5, 7, [15, 87], 9], 7], 18));
