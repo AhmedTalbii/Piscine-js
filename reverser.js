13694290af0d56938cd87ced13d6cf0f3d89d682
Ahmed Talbi <ahtalbi@noreply.learn.zone01oujda.ma>
2025-11-29 11:32:55 +0000

    Update reverser.js

--- reverser.js
+++ reverser.js
@@ -1,6 +1,6 @@
 function reverse(ele) {
     let iA = Array.isArray(ele), arr = iA ? ele : ele.split("");
-    let s = 0, e = ele.length;
+    let s = 0, e = ele.length-1;
     while (s < e) {
         [arr[s], arr[e]] = [arr[e], arr[s]];
         s++;
