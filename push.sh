1aa770b9919f3dc2cadd30890350af550b65a883
AhmedTalbii <ahmedtalbi459@gmail.com>
2025-11-30 11:01:11 +0100

    script push + test

--- push.sh
+++ push.sh
@@ -6,12 +6,16 @@ MSG=$1
 git remote add github https://github.com/AhmedTalbii/Piscine-js.git 2>/dev/null
 git remote add gitea https://learn.zone01oujda.ma/git/ahtalbi/piscine-js.git 2>/dev/null
 
-# Commit changes
+# Stage changes
 git add .
-git commit -m "$MSG"
 
-# Push to GitHub
-git -c user.name="AhmedTalbii" -c user.email="ahmedtalbi459@gmail.com" push github
+# Commit and push to GitHub
+git -c user.name="AhmedTalbii" -c user.email="ahmedtalbi459@gmail.com" commit -m "$MSG"
+git push github
 
-# Push to Gitea
-git -c user.name="ahtalbi" -c user.email="obetox1@gmail.com" push gitea
+# Reset last commit but keep changes staged
+git reset --soft HEAD~1
+
+# Commit and push to Gitea
+git -c user.name="ahtalbi" -c user.email="obetox1@gmail.com" commit -m "$MSG"
+git push gitea
