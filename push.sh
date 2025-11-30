352fe6c3ce2c2abc83cdf33ba2c77a6dafb09fc1
ahtalbi <obetox1@gmail.com>
2025-11-30 10:58:37 +0100

    script push + test

--- push.sh
+++ push.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
-
 set -x
 MSG=$1
 
-# GitHub config
-git remote set-url origin https://github.com/AhmedTalbii/Piscine-js.git
-git config --global user.name "AhmedTalbii"
-git config --global user.email "ahmedtalbi459@gmail.com"
-git config --global credential.helper store
+# Add remotes if not already added
+git remote add github https://github.com/AhmedTalbii/Piscine-js.git 2>/dev/null
+git remote add gitea https://learn.zone01oujda.ma/git/ahtalbi/piscine-js.git 2>/dev/null
+
+# Commit changes
 git add .
 git commit -m "$MSG"
-git push
 
-# Gitea config
-git remote set-url origin https://learn.zone01oujda.ma/git/ahtalbi/piscine-js.git
-git config --global user.name "ahtalbi"
-git config --global user.email "obetox1@gmail.com"
-git config --global credential.helper store
-git push
+# Push to GitHub
+git -c user.name="AhmedTalbii" -c user.email="ahmedtalbi459@gmail.com" push github
+
+# Push to Gitea
+git -c user.name="ahtalbi" -c user.email="obetox1@gmail.com" push gitea
