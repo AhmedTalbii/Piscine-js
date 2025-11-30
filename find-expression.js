de7295898f63dd51cae1a861a19ae667a9318ba0
AhmedTalbii <ahmedtalbi459@gmail.com>
2025-11-30 09:20:24 +0100

    find expression

--- find-expression.js
+++ find-expression.js
@@ -1,16 +1,18 @@
 function findExpression(n) {
-    let res = "1";
+    let res = [];
     const rec = (i = 1) => {
         if (i === n) return true;
-        if (i * 2 <= n && rec(i * 2)) {
-            res += " "+add4;
+        if (i * 2 <= n && rec(i * 2) && i + 4 !== n) {
+            res.unshift(mul2);
             return true;
-        }
-        if (i + 4 <= n && rec(i + 4)) {
-            res += " "+mul2;
+        } else if (i + 4 <= n && rec(i + 4)) {
+            res.unshift(add4);
             return true;
         }
         return false;
     }
-    return (rec()) ? res : undefined;
+    res = [...res.reverse()];
+    res = (rec()) ? res : undefined;
+    if (res) res.unshift("1");
+    return res;
 }
