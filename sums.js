cee47ca1cd393135ff5b919091b922640a7561e0
AhmedTalbii <ahmedtalbi459@gmail.com>
2025-11-30 10:04:21 +0100

    sums

--- sums.js
+++ sums.js
@@ -1,5 +1,19 @@
 function sums(n) {
-    let rec = () => {
-
+    let res = [];
+    let mArr = [];
+    let exists = new Set();
+    let rec = (c = 0) => {
+        if (c > n) return;
+        for (let i = 1; i < n; i++) {
+            if (mArr.reduce((acc, ele) => acc + ele, 0) === n && !exists.has([...mArr].sort().join(""))) {
+                exists.add([...mArr].sort().join(""));
+                res.push([...mArr]);
+            };
+            mArr.push(i);
+            rec(c+1);
+            mArr.pop();
+        }
     }
+    rec();
+    console.log(res);
 }
